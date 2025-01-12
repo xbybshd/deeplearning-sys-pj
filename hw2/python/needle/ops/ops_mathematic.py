04d84bcf67740c4e5c558574e847cf57ccda437f
peiyungong <pyg315315@163.com>
2025-01-13 02:36:32 +0800

    hw2

---
+++ hw2/python/needle/ops/ops_mathematic.py
@@ -0,0 +1,379 @@
+"""Operator implementations."""
+
+from numbers import Number
+from typing import Optional, List, Tuple, Union
+
+from ..autograd import NDArray
+from ..autograd import Op, Tensor, Value, TensorOp
+from ..autograd import TensorTuple, TensorTupleOp
+import numpy
+
+# NOTE: we will import numpy as the array_api
+# as the backend for our computations, this line will change in later homeworks
+
+BACKEND = "np"
+import numpy as array_api
+
+class EWiseAdd(TensorOp):
+    def compute(self, a: NDArray, b: NDArray):
+        return a + b
+
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        return out_grad, out_grad
+
+
+def add(a, b):
+    return EWiseAdd()(a, b)
+
+
+class AddScalar(TensorOp):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a: NDArray):
+        return a + self.scalar
+
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        return out_grad
+
+
+def add_scalar(a, scalar):
+    return AddScalar(scalar)(a)
+
+
+class EWiseMul(TensorOp):
+    def compute(self, a: NDArray, b: NDArray):
+        return a * b
+
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        lhs, rhs = node.inputs
+        return out_grad * rhs, out_grad * lhs
+
+
+def multiply(a, b):
+    return EWiseMul()(a, b)
+
+
+class MulScalar(TensorOp):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a: NDArray):
+        return a * self.scalar
+
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        return (out_grad * self.scalar,)
+
+
+def mul_scalar(a, scalar):
+    return MulScalar(scalar)(a)
+
+
+class EWisePow(TensorOp):
+    """Op to element-wise raise a tensor to a power."""
+
+    def compute(self, a: NDArray, b: NDArray) -> NDArray:
+        ### BEGIN YOUR SOLUTION
+        return a**b
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+        
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        # if not isinstance(node.inputs[0], NDArray) or not isinstance(
+        #     node.inputs[1], NDArray
+        # ):
+        #     raise ValueError("Both inputs must be tensors (NDArray).")
+        a, b = node.inputs[0], node.inputs[1]
+        grad_a = out_grad * b * (a **(b-1))
+        grad_b = out_grad * (a ** b) * (log(a.data))
+        return grad_a, grad_b
+        ### END YOUR SOLUTION
+
+def power(a, b):
+    return EWisePow()(a, b)
+
+
+class PowerScalar(TensorOp):
+    """Op raise a tensor to an (integer) power."""
+
+    def __init__(self, scalar: int):
+        self.scalar = scalar
+
+    def compute(self, a: NDArray) -> NDArray:
+        ### BEGIN YOUR SOLUTION
+        return array_api.power(a,self.scalar)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        a = node.inputs[0]
+        return self.scalar * (power_scalar(a, self.scalar-1)) * out_grad
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def power_scalar(a, scalar):
+    return PowerScalar(scalar)(a)
+
+
+class EWiseDiv(TensorOp):
+    """Op to element-wise divide two nodes."""
+
+    def compute(self, a, b):
+        ### BEGIN YOUR SOLUTION
+        return array_api.divide(a,b)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        a,b = node.inputs
+        return out_grad/b, -a*out_grad/(b*b)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def divide(a, b):
+    return EWiseDiv()(a, b)
+
+
+class DivScalar(TensorOp):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.divide(a,self.scalar)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return out_grad/self.scalar
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def divide_scalar(a, scalar):
+    return DivScalar(scalar)(a)
+
+
+class Transpose(TensorOp):
+    def __init__(self, axes: Optional[tuple] = None):
+        self.axes = axes
+
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        if self.axes:
+            return array_api.swapaxes(a, self.axes[0], self.axes[1])
+        else:
+            return array_api.swapaxes(a, a.ndim-2, a.ndim-1)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return out_grad.transpose(self.axes)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def transpose(a, axes=None):
+    return Transpose(axes)(a)
+
+
+class Reshape(TensorOp):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.reshape(a, self.shape)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return out_grad.reshape(node.inputs[0].shape)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def reshape(a, shape):
+    return Reshape(shape)(a)
+
+
+class BroadcastTo(TensorOp):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.broadcast_to(a, self.shape)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        # 获取输入张量的原始形状
+        ori_shape = node.inputs[0].shape
+
+        # 创建一个列表，用于标记哪些维度需要在反向传播中“缩减”
+        shrink_dims = [i for i in range(len(self.shape))]
+        
+        # 遍历输入和广播后的形状，判断哪些维度需要进行求和（即“缩减”）
+        for i, (ori, cur) in enumerate(zip(reversed(ori_shape), reversed(self.shape))):
+            if ori == cur:
+                shrink_dims[len(self.shape) - i - 1] = -1  # 如果对应的维度大小相同，就不需要缩减
+
+        # 过滤掉所有不需要缩减的维度（即保留需要求和的维度）
+        shrink_dims = tuple(filter(lambda x: x >= 0, shrink_dims))
+        
+        # 将输出的梯度按照需要缩减的维度进行求和，并且重新调整形状为原始输入张量的形状
+        return out_grad.sum(shrink_dims).reshape(ori_shape)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def broadcast_to(a, shape):
+    return BroadcastTo(shape)(a)
+
+
+class Summation(TensorOp):
+    def __init__(self, axes: Optional[tuple] = None):
+        self.axes = axes
+
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.sum(a, self.axes)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        # 获取算子输入矩阵的维度形状，是一个list，元素是每维度
+        new_shape = list(node.inputs[0].shape)
+        # 类似三元运算符，如果此算子求和时没标明维度，说明会对所有维度都求和，如果标明维度，那就只求和标明的维度
+        axes = range(len(new_shape)) if self.axes is None else self.axes
+        # 对要求和的维度里，在new_shape list中标明1
+        for axis in axes:
+            new_shape[axis] = 1
+        # 被求和的维度在输出向量的形状中是被删除了，new_shape是在反向回来的梯度矩阵上进行reshape，把删除的维度恢复为1，之后再1的基础上
+        # 广播出真正的维度
+        return out_grad.reshape(new_shape).broadcast_to(node.inputs[0].shape)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def summation(a, axes=None):
+    return Summation(axes)(a)
+
+
+class MatMul(TensorOp):
+    def compute(self, a, b):
+        ### BEGIN YOUR SOLUTION
+        return array_api.matmul(a, b)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        lhs, rhs = node.inputs
+        lgrad, rgrad = matmul(out_grad, rhs.transpose()), matmul(lhs.transpose(), out_grad)
+        # 先保证最后两个维度相同，前面的成为批量维度，批量维度不同，则对小的数组加一维度进行拓展
+        # 如果维度相同，且一个数组维度数值是1，则进行拓展，除此之外认为错误
+        # 广播机制：
+        # 如果两个数组的维度不同，NumPy 会在维度较小的数组的前面加上 1，直到两个数组的维度相同。
+        # 在对齐维度时，如果某个维度的大小是 1，NumPy 会通过扩展该维度来匹配另一个数组的维度。
+        # 如果两个数组在某一维度上的大小不相同，并且该维度上的大小都不为 1，则广播会失败，抛出错误。
+        # 当因为维度数量不同而拓展时，在反向要将这一维度reduce掉
+        if len(lhs.shape) < len(lgrad.shape):
+            lgrad = lgrad.sum(tuple([i for i in range(len(lgrad.shape) - len(lhs.shape))]))
+        if len(rhs.shape) < len(rgrad.shape):
+            rgrad = rgrad.sum(tuple([i for i in range(len(rgrad.shape) - len(rhs.shape))]))
+        return lgrad, rgrad
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def matmul(a, b):
+    return MatMul()(a, b)
+
+
+class Negate(TensorOp):
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.negative(a)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return - out_grad
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def negate(a):
+    return Negate()(a)
+
+
+class Log(TensorOp):
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.log(a)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return out_grad / node.inputs[0]
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def log(a):
+    return Log()(a)
+
+
+class Exp(TensorOp):
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.exp(a)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        return out_grad * exp(node.inputs[0])
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def exp(a):
+    return Exp()(a)
+
+
+class ReLU(TensorOp):
+    def compute(self, a):
+        ### BEGIN YOUR SOLUTION
+        return array_api.maximum(0, a)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        # realize_cached_data 是因为我要numpy,copy是因为需要直接在数据上修改，用out是因为如果用input，要再处理一遍小于0
+        out = node.realize_cached_data().copy()
+        # out 所有大于0的元素，为ture，也构成了索引，然后索引里面为1的都是1
+        out[out > 0] = 1
+        return out_grad * Tensor(out)
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+
+def relu(a):
+    return ReLU()(a)
+

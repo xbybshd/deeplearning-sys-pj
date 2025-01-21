be5a580f59812b7e91497484849f8053b0dd4981
xbybshd <1596439051@qq.com>
2025-01-22 04:37:31 +0800

    hw3-linux-cmake

---
+++ hw3/CMakeFiles/ndarray_backend_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ndarray_backend_cuda.dir/src/ndarray_backend_cuda_generated_ndarray_backend_cuda.cu.o"
+  "python/needle/backend_ndarray/ndarray_backend_cuda.cpython-36m-x86_64-linux-gnu.so"
+  "python/needle/backend_ndarray/ndarray_backend_cuda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ndarray_backend_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
